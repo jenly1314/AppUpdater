b68626587ab293d996fb284f296dea89d7bfee60
jenly1314 <jenly1314@mail.com>
2019-11-25 16:06:25 +0800

    CI

--- app/src/main/java/com/king/appupdater/MainActivity.java
+++ app/src/main/java/com/king/appupdater/MainActivity.java
@@ -258,7 +258,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void OnClick(View v){
+    public void onClick(View v){
         switch (v.getId()){
             case R.id.btn1:
                 clickBtn1();
